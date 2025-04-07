873473f0cda31a6e506ae3f6b318049c805f743a
System Administrator <root@Jesse.local>
2025-04-06 19:06:58 -0600

    homebrew-cask

--- Formula/modelmake.rb
+++ Formula/modelmake.rb
@@ -2,17 +2,17 @@ class Modelmake < Formula
   desc "3D Model Conversion, Scaling, and Optimization Tool"
   homepage "https://github.com/sudo-self/makemodel-homebrew"
   url "https://github.com/sudo-self/makemodel-homebrew/blob/main/modelmake.tar.gz", using: :curl
-  sha256 "INSERT_SHA256_OF_TARBALL" # Replace with the actual sha256 hash of the tarball
+  sha256 "26d564a629d69dfdd4c4d22c61ed3df160e94a46b483350244e8553917370f7e"
 
-  depends_on "python@3.9" # Adjust the python version if necessary
+  depends_on "python@3.9"
 
   def install
-    # Extract the contents of the tarball into the installation directory
+ 
     bin.install Dir["*"]
   end
 
   test do
-    # Ensure the installed program works by running a simple test
+    
     system "#{bin}/modelmake", "--help"
   end
 end
