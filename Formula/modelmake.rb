1171450ca6c64006c702cffebcab99d5a042d020
System Administrator <root@Jesse.local>
2025-04-06 19:12:40 -0600

    homebrew

--- Formula/modelmake.rb
+++ Formula/modelmake.rb
@@ -1,18 +1,19 @@
 class Modelmake < Formula
   desc "3D Model Conversion, Scaling, and Optimization Tool"
   homepage "https://github.com/sudo-self/makemodel-homebrew"
-  url "https://github.com/sudo-self/makemodel-homebrew/blob/main/modelmake.tar.gz", using: :curl
-  sha256 "26d564a629d69dfdd4c4d22c61ed3df160e94a46b483350244e8553917370f7e"
+  url "https://github.com/sudo-self/makemodel-homebrew/raw/main/modelmake.tar.gz", using: :curl
+  sha256 "26d564a629d69dfdd4c4d22c61ed3df160e94a46b483350244e8553917370f7e"  # Ensure this SHA256 is correct
 
   depends_on "python@3.9"
 
   def install
- 
-    bin.install Dir["*"]
+    # Install the script in the bin directory
+    bin.install "modelmake.py" => "modelmake"
   end
 
   test do
-    
+    # Test that the tool runs and outputs help information
     system "#{bin}/modelmake", "--help"
   end
 end
+
