8470cca6793510b36fb953ae62f7424b9ad767fa
System Administrator <root@Jesse.local>
2025-04-06 19:18:20 -0600

    homeb

--- Formula/modelmake.rb
+++ Formula/modelmake.rb
@@ -3,7 +3,7 @@ class Modelmake < Formula
   homepage "https://github.com/sudo-self/makemodel-homebrew"
   url "https://github.com/sudo-self/makemodel-homebrew/raw/main/modelmake.tar.gz", using: :curl
   sha256 "26d564a629d69dfdd4c4d22c61ed3df160e94a46b483350244e8553917370f7e"
-  version "2023.04.06"  # Pseudo version or use a commit hash
+  version "2023.04.06"  # This could also be a Git commit hash or timestamp, but it's important to have a version
 
   depends_on "python@3.9"
 
@@ -15,5 +15,6 @@ class Modelmake < Formula
     system "#{bin}/modelmake", "--help"
   end
 end
+
 
 
